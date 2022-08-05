46cd74cfb5f890a5bc6d03f259c69b83c8bfbc79
Lupu Matei <mateolupu30@gmail.com>
2022-08-05 15:55:10 -0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+import os
+os.system('clear')
+
+first_name = 'Lupu'
+last_name = 'Matei'
+
+#strings
+#numere
+#liste
+#tuples
+#dicitonare
+#boolean
+
+varsta = 16
+
+fav_pizza = {
+	'Stefan': 'Salami',
+	'Mihai': 'Stagioni'		
+}
+
+name = False
+
+
+print (name)
